f24150635c6e8a9737225a7a7a718e764e4ac93b
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2025-09-18 15:28:34 +0000

    ham

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-// tailwind.config.js
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: []
-};
+
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ }
